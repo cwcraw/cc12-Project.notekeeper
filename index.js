a1d368292800eba2d8fb341c7c2fe4405d7bf414
cwcraw <cwcraw@gmail.com>
2020-04-25 20:14:17 +0900

    Confused

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
   const taskList = document.createElement("div");
   taskList.id = "taskList";
-  // taskList.innerHTML =
+  taskList.innerHTML = console.log(taskList); //localhost:4000/graphql?query=%7B%20tasklist%7B%0A%20%20id%0A%7D%0A%7D
   document.body.append(taskList);
 });
